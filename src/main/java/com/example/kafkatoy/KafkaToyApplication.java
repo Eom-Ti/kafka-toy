5dfd61be25ac792ed36f0deb18100f72fed47141
엄태권 <akfls221@gmail.com>
2025-09-25 02:31:36 +0900

    Initial commit

---
+++ src/main/java/com/example/kafkatoy/KafkaToyApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkatoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaToyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaToyApplication.class, args);
+    }
+
+}
